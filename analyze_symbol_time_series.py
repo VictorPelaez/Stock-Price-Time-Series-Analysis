7afedf2fa13a86dcb2b94903600a33588db14e91
Chris Diehl <cpdiehl@gmail.com>
2011-06-08 12:19:34 -0700

    Added code to compute performance statistics based on the 50 day moving average of the closing price and plot the moving average. Work in progress.

--- analyze_symbol_time_series.py
+++ analyze_symbol_time_series.py
@@ -1,5 +1,9 @@
-from datetime import date
-from dateutil import parser
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab   as mlab
+import matplotlib.ticker as ticker
+from datetime    import date
+from dateutil    import parser
 from ystockquote import *
 
 # Load the symbols to analyze
@@ -17,7 +21,7 @@ for symbol in symbols:
     symbol_data = history[symbol][1:]
 
     # Convert string data to properly typed data
-    history[symbol] = []
+    history[symbol] = history[symbol][:1]
     for element in symbol_data:
         new_element = []
 
@@ -29,3 +33,73 @@ for symbol in symbols:
 
         # Add the list back to the history
         history[symbol].append(new_element)
+
+# Compute 50 day moving averages of closing price
+fifty_day_ma_history = {}
+for symbol in symbols:
+    fifty_day_ma_history[symbol] = []
+
+    # Get closing price history for symbol
+    closing_prices = map(lambda e : e[4], history[symbol][1:])
+
+    # Compute moving averages
+    fifty_day_window = closing_prices[:50]
+    for i in xrange(len(closing_prices)-49):
+        fifty_day_ma_history[symbol].append(sum(fifty_day_window)/50.0)
+
+        # Update the window as necessary
+        if i < len(closing_prices)-50:
+            del fifty_day_window[0]
+            fifty_day_window.append(closing_prices[50+i])
+
+# Compute 3 month, 6 month and 1 year performance based on smoothed closing prices
+# along with the relative strength. Will use 13 week, 26 week and 52 week lags for 
+# computations. Relative strength is defined here as the average of the 3 month, 
+# 6 month and 1 year performance.
+performance = {}
+for symbol in symbols:
+    symbol_perf = {}
+    smoothed_price = fifty_day_ma_history[symbol]
+    symbol_perf['3mo'] = (smoothed_price[0] - smoothed_price[5*13]) / smoothed_price[5*13]
+    symbol_perf['6mo'] = (smoothed_price[0] - smoothed_price[5*26]) / smoothed_price[5*26]
+    symbol_perf['1yr'] = (smoothed_price[0] - smoothed_price[5*52]) / smoothed_price[5*52]
+    symbol_perf['rs']  = (symbol_perf['3mo'] + symbol_perf['6mo'] + symbol_perf['1yr']) / 3.0
+    performance[symbol] = symbol_perf
+
+# Sort the symbols based on relative strength
+rs = []
+for symbol in symbols:
+    rs.append((symbol,performance[symbol]['rs']))
+rs.sort(key = lambda tup : tup[1],reverse = True)
+rs_ordered_symbols = map(lambda tup : tup[0],rs)
+
+# Print out the performance figures
+print "3 MO\t6 MO\t1 YR\tRS\tSYMB"
+for symbol in rs_ordered_symbols:
+    keys = ['3mo','6mo','1yr','rs']
+    s = ''
+    for key in keys:
+        s += "%.1f\t" % (performance[symbol][key]*100.0)
+    s += "%s" % symbol
+    print s
+
+# Plot the fifty day moving averages
+for symbol in symbols:
+    ma_history = fifty_day_ma_history[symbol]
+    N = len(ma_history)
+    ind = np.arange(N)
+    dates = map(lambda tup : tup[0],history[symbol][1:N+1])
+
+    def format_date(x, pos=None):
+        thisind = np.clip(int(x+0.5), 0, N-1)
+        return dates[thisind].strftime('%Y-%m-%d')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(ind, ma_history, 'o-')
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    fig.autofmt_xdate()
+
+    plt.draw()
+    
+
